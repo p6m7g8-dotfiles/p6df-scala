84d2e16d43f8126f84f01ce1949e64f498697157
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:29:10 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,12 @@
+p6df::modules::scala::version() { echo "0.0.1" }
+p6df::modules::scala::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::scala::external::brew() {
+}
+
+p6df::modules::scala::init() {
+}
+
+p6df::modules::scala::init
