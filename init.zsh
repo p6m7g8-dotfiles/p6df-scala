42d394fcc0d9df1b63d137fabbb4e505ee19c165
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-27 21:05:51 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 p6df::modules::scala::version() { echo "0.0.1" }
-p6df::modules::scala::deps()    { 
+p6df::modules::scala::deps()    {
 	ModuleDeps=(
 	)
 }
@@ -9,17 +9,19 @@ p6df::modules::scala::external::brew() {
 
 p6df::modules::scala::init() {
 
-  p6df::modules::scala::scalaenv::init
+#  p6df::modules::scala::scalaenv::init
 }
 
 p6df::modules::scala::scalaenv::init() {
     [ -n "$DISABLE_ENVS" ] && return
 
-    export SCALAENV_ROOT=/Users/pgollucci/.local/share/scalaenv/scalaenv
-    p6dfz::util::path_if $SCALAENV_ROOT/bin
+    SCALAENV_ROOT=/Users/pgollucci/.local/share/scalaenv/scalaenv
 
     if [ -x $SCALAENV_ROOT/bin/scalaenv ]; then
+      export SCALENV_ROOT
       export HAS_SCALAENV=1
+
+      p6dfz::util::path_if $SCALAENV_ROOT/bin
       eval "$(scalaenv init - zsh)"
     fi
 }
