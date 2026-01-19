4fe0ef001a164389af5d73d37bd82c99a3313903
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-18 22:18:52 -0500

    chore: converts to new prompt v4 system etc (#10)

--- init.zsh
+++ init.zsh
@@ -24,12 +24,12 @@ p6df::modules::scala::langs() {
 
   local previous
   previous=$(p6df::modules::scala::scalaenv::latest::installed)
-  scalaenv uninstall -f $previous
+  scalaenv uninstall -f "$previous"
 
   local latest
   latest=$(p6df::modules::scala::scalaenv::latest)
-  scalaenv install -f $latest
-  scalaenv global $latest
+  scalaenv install -f "$latest"
+  scalaenv global "$latest"
   scalaenv rehash
 
   p6_return_void
@@ -61,17 +61,47 @@ p6df::modules::scala::init() {
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::scala::env::prompt::info()
+# Function: str str = p6df::modules::scala::prompt::env()
 #
 #  Returns:
 #	str - str
 #
-#  Environment:	 SCALAENV_ROOT
 #>
 ######################################################################
-p6df::modules::scala::env::prompt::info() {
+p6df::modules::scala::prompt::env() {
 
-  local str="scalaenv_root:\t  $SCALAENV_ROOT"
+#  local str="scalaenv_root:\t  $SCALAENV_ROOT"
+  local str=""
 
   p6_return_str "$str"
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::scala::prompt::lang()
+#
+#>
+######################################################################
+p6df::modules::scala::prompt::lang() {
+
+  local ver
+
+  local ver_mgr
+  ver_mgr=$(scalaenv version-name 2>/dev/null)
+  if p6_string_eq "$ver_mgr" "system"; then
+    local ver_sys="sys@"
+    local v
+    v=$(scala -nc -version 2>&1 | awk '{print $5}')
+    if p6_string_blank "$v"; then
+      ver_sys="sys:no"
+    fi
+    ver="$ver_sys"
+  else
+    ver="$ver_mgr"
+  fi
+
+  local str="scala:$ver"
+
+  p6_return "$str"
 }
